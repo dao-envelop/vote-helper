4274e9dce0fdedf8e7bc52f9ad2df2e082ab3c78
Alex <alexamuek@gmail.com>
2025-07-07 22:25:31 +0800

    add tests

---
+++ src/IEnvelopwNFT721.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.28;
+
+import "./LibEnvelopTypes.sol";
+
+interface IEnvelopwNFT721 {
+
+    function wnftInfo(uint256 tokenId) external view returns (ETypes.WNFT memory);
+}
--- test/EnvelopSBTV1VoteHelper.t.sol
+++ test/EnvelopSBTV1VoteHelper.t.sol
@@ -3,15 +3,20 @@ pragma solidity ^0.8.13;
 
 import {Test, console2} from "forge-std/Test.sol";
 import "../src/EnvelopSBTV1VoteHelper.sol";
+import "../src/IEnvelopwNFT721.sol";
 
 contract EnvelopSBTV1VoteHelperTest is Test {
     EnvelopSBTV1VoteHelper public voteHelper;
     address constant public NIFTSY = 0x432cdbC749FD96AA35e1dC27765b23fDCc8F5cf1;
     address constant public SBT = 0xf2B919B54848639e552C697D6F39a544AC6D2328;
+    address constant public user1 = 0x39cc948dA00d2E2a09Ed4D508768a92e0A3BBE6A;
+    address constant public user2 = 0xF8E02D473710506DB5D9210D96725cccbf4137c9;
+    address constant public user3 = 0xf315B9006C20913D6D8498BDf657E778d4Ddf2c4;
+    address constant public voterAddress = 0xBDb5201565925AE934A5622F0E7091aFFceed5EB;
 
 
     function setUp() public {
-        voteHelper = EnvelopSBTV1VoteHelper(SBT);
+        voteHelper = EnvelopSBTV1VoteHelper(voterAddress);
     }
 
     function test_network_forked() public view{
@@ -27,32 +32,27 @@ contract EnvelopSBTV1VoteHelperTest is Test {
         }
     }
 
+    function test_one_sbt_and_balance() public view {
+        uint256 niftsyBalance = IERC20Read(NIFTSY).balanceOf(user1);
+        ETypes.WNFT memory wnft = IEnvelopwNFT721(SBT).wnftInfo(1);
+        uint256 collateral = wnft.collateral[0].amount;
+        uint256 expectedAmount = niftsyBalance + voteHelper.DEFAULT_WRAPPED_MULTIPLIER() * collateral;
+        uint256 actualAmount = voteHelper.balanceOf(user1);
+        assertEq(expectedAmount, actualAmount);
+    }
 
-    // function test_getProtocolMetricsByAddress() public view {
-    //     DD.ProtocolMetrics memory _m;
-        
-    //     for (uint8 i = 0; i < markets.length; i ++) {
-    //         _m = flLiqHelper.getProtocolMetricsByAddress(markets[i]);
+    function test_without_any_balance() public view {
+        uint256 niftsyBalance = IERC20Read(NIFTSY).balanceOf(user2);
+        uint256 expectedAmount = niftsyBalance;
+        uint256 actualAmount = voteHelper.balanceOf(user2);
+        assertEq(expectedAmount, actualAmount);
+    }
 
-    //         console2.log(
-    //            "TVL     in %s = %s, %s", 
-    //             ERC20(markets[i]).name(),
-    //             _m.metrics[0].value, _m.metrics[0].scale
-    //         );
-    //         console2.log(
-    //            "Credits in %s = %s, %s", 
-    //             ERC20(markets[i]).name(),
-    //              _m.metrics[1].value, _m.metrics[1].scale
-    //         );
-    //         console2.log(
-    //            "Full APR in %s = %s, %s", 
-    //             ERC20(markets[i]).name(),
-    //              _m.metrics[2].value, _m.metrics[2].scale
-    //         );
-    //         //assertEq(_m.metrics[2].value, aavev3Helper.getReserveData(markets[i]).currentLiquidityRate);
-    //         //assertEq(_m.metrics[3].value -_m.metrics[2].value, cv3Helper.getRewardAprForSupplyBase(markets[i], cv3Helper.REWARD_PRICE_FEED()));
-            
-    //     }
-    // }
-
+    function test_only_balance() public view {
+        uint256 niftsyBalance = IERC20Read(NIFTSY).balanceOf(user3);
+        uint256 expectedAmount = niftsyBalance;
+        assertGt(expectedAmount, 1);
+        uint256 actualAmount = voteHelper.balanceOf(user3);
+        assertEq(expectedAmount, actualAmount);
+    }
 }
